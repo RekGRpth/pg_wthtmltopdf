6fe0aab9f5175c1e8b662dc18149438f0245bc00
RekGRpth <rekgrpth@gmail.com>
2019-07-05 13:12:02 +0500

    up

--- pg_html2pdf.c
+++ pg_html2pdf.c
@@ -23,6 +23,7 @@ EXTENSION(html2pdf) {
     HPDF_Page page;
     HPDF_UINT32 size;
     HPDF_BYTE *buf = NULL;
+    HPDF_STATUS hs = 0;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("html is null!")));
     html = TextDatumGetCString(PG_GETARG_DATUM(0));
     if (!(pdf = HPDF_NewEx(error_handler, (HPDF_Alloc_Func)palloc, pfree, 0, NULL))) ereport(ERROR, (errmsg("!pdf")));
@@ -35,17 +36,21 @@ EXTENSION(html2pdf) {
     if (!(size = HPDF_GetStreamSize(pdf))) goto HPDF_Free;
     elog(LOG, "size = %u", size);
     if (!(buf = palloc(size))) goto HPDF_Free;
-    switch (HPDF_ReadFromStream(pdf, buf, &size)) {
+    switch ((hs = HPDF_ReadFromStream(pdf, buf, &size))) {
         case HPDF_OK: break;
         case HPDF_STREAM_EOF: break;
         default: goto HPDF_Free;
     }
 HPDF_Free:
+//    elog(LOG, "size = %u", size);
+//    elog(LOG, "hs = %lu", hs);
     page = HPDF_GetCurrentPage(pdf);
     if (page) while (HPDF_Page_GetGStateDepth(page) > 1) HPDF_Page_GRestore(page);
     (void)HPDF_Free(pdf);
     (void)pfree(html);
+    elog(LOG, "buf = %*.*s", size, size, (const char *)buf);
     elog(LOG, "strlen(buf) = %lu", strlen((const char *)buf));
+    elog(LOG, "size = %u", size);
 //    PG_RETURN_TEXT_P(cstring_to_text_with_len((const char *)buf, size));
     return CStringGetTextDatum((const char *)buf);
 }
