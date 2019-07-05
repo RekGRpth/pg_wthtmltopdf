2d53f80a975f9ac27a6f5f611fc794db0d9dc8f4
RekGRpth <rekgrpth@gmail.com>
2019-07-05 16:36:28 +0500

    up

--- pg_html2pdf.c
+++ pg_html2pdf.c
@@ -1,6 +1,8 @@
 #include <postgres.h>
 #include <fmgr.h>
 
+#include <catalog/pg_type.h>
+#include <libpq/pqformat.h>
 #include <utils/builtins.h>
 
 #include "MyWPdfRenderer.h"
@@ -23,6 +25,8 @@ EXTENSION(html2pdf) {
     HPDF_Page page;
     HPDF_UINT32 size;
     HPDF_BYTE *buf = NULL;
+    text *t;
+    StringInfoData sid;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("html is null!")));
     html = TextDatumGetCString(PG_GETARG_DATUM(0));
     if (!(pdf = HPDF_NewEx(error_handler, (HPDF_Alloc_Func)palloc, pfree, 0, NULL))) ereport(ERROR, (errmsg("!pdf")));
@@ -33,7 +37,6 @@ EXTENSION(html2pdf) {
     if (!MyWPdfRenderer_render(error, pdf, page, html)) goto HPDF_Free;
     if (HPDF_SaveToStream(pdf) != HPDF_OK) goto HPDF_Free;
     if (!(size = HPDF_GetStreamSize(pdf))) goto HPDF_Free;
-//    elog(LOG, "size = %u", size);
     if (!(buf = palloc(size))) goto HPDF_Free;
     switch (HPDF_ReadFromStream(pdf, buf, &size)) {
         case HPDF_OK: break;
@@ -45,9 +48,9 @@ HPDF_Free:
     if (page) while (HPDF_Page_GetGStateDepth(page) > 1) HPDF_Page_GRestore(page);
     (void)HPDF_Free(pdf);
     (void)pfree(html);
-//    elog(LOG, "buf = %s", (const char *)buf + strlen((const char *)buf) + 1);
-//    elog(LOG, "strlen(buf) = %lu", strlen((const char *)buf));
-//    elog(LOG, "size = %u", size);
     PG_RETURN_TEXT_P(cstring_to_text_with_len((const char *)buf, size));
-//    return CStringGetTextDatum((const char *)buf);
+    t = cstring_to_text_with_len((const char *)buf, size);
+    pq_begintypsend(&sid);
+    pq_sendtext(&sid, VARDATA_ANY(t), VARSIZE_ANY_EXHDR(t));
+    PG_RETURN_BYTEA_P(pq_endtypsend(&sid));
 }
