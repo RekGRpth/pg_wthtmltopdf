ccf6dfa9a0ccf70471c04585a977a23b6e9a329a
RekGRpth <rekgrpth@gmail.com>
2019-07-06 10:28:43 +0500

    up

--- pg_html2pdf.c
+++ pg_html2pdf.c
@@ -24,9 +24,7 @@ EXTENSION(html2pdf) {
     HPDF_Doc pdf;
     HPDF_Page page;
     HPDF_UINT32 size;
-    HPDF_BYTE *buf = NULL;
-    text *t;
-    StringInfoData sid;
+    text *result = NULL;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("html is null!")));
     html = TextDatumGetCString(PG_GETARG_DATUM(0));
     if (!(pdf = HPDF_NewEx(error_handler, (HPDF_Alloc_Func)palloc, pfree, 0, NULL))) ereport(ERROR, (errmsg("!pdf")));
@@ -37,19 +35,18 @@ EXTENSION(html2pdf) {
     if (!MyWPdfRenderer_render(error, pdf, page, html)) goto HPDF_Free;
     if (HPDF_SaveToStream(pdf) != HPDF_OK) goto HPDF_Free;
     if (!(size = HPDF_GetStreamSize(pdf))) goto HPDF_Free;
-    if (!(buf = palloc(size))) goto HPDF_Free;
-    switch (HPDF_ReadFromStream(pdf, buf, &size)) {
+    if (!(result = palloc(size + VARHDRSZ))) goto HPDF_Free;
+    SET_VARSIZE(result, size + VARHDRSZ);
+    switch (HPDF_ReadFromStream(pdf, (HPDF_BYTE *)VARDATA(result), &size)) {
         case HPDF_OK: break;
         case HPDF_STREAM_EOF: break;
         default: goto HPDF_Free;
     }
 HPDF_Free:
+    elog(LOG, "size = %u", size);
     page = HPDF_GetCurrentPage(pdf);
     if (page) while (HPDF_Page_GetGStateDepth(page) > 1) HPDF_Page_GRestore(page);
     (void)HPDF_Free(pdf);
     (void)pfree(html);
-    t = cstring_to_text_with_len((const char *)buf, size);
-    pq_begintypsend(&sid);
-    pq_sendtext(&sid, VARDATA_ANY(t), VARSIZE_ANY_EXHDR(t));
-    PG_RETURN_BYTEA_P(pq_endtypsend(&sid));
+    PG_RETURN_TEXT_P(result);
 }
